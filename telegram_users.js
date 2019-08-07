5fa9861bf41b73f35943117e631ce6b983f721ba
AtakanErmis <ataerms@gmail.com>
2019-08-07 15:09:37 +0300

    Güvenlik açığı

--- telegram_users.js
+++ telegram_users.js
@@ -25,38 +25,49 @@ const removeJoinedHandler = async (ctx) => {
   await ctx.deleteMessage().catch(() => console.log("Hata: Yetkisiz İşlem. Lütfen eksiCodeBot'a yönetici ayrıcalıkları tanıyın."))
 }
 
-bot.command((ctx) => {
-    const args = ctx.state.command.splitArgs;
-    const url = args[0].startsWith("http") ? args[0] : "http://" + args[0]
-    fetch(url)
-      .then(res => res.text())
-      .then(html => {
-        const title = parse(html).querySelector('title').rawText;
-        const requestData = {
-          "doc_name": title,
-          "doc_link": url,
-          "doc_creator_tg": ctx.from.id
-        }
-        fetch("http://api.eksicode.org/kaynaklars", {
-          "method": "POST",
-          "headers": {
-            "Content-Type": "application/json"
-          },
-          "body": JSON.stringify(requestData)
-        })
-        .then(res => res.json())
-        .then(() => {
-          ctx.reply(`Teşekkürler ${ctx.from.first_name}! Yönetici ekibimiz en kısa zamanda inceleyip onaylayacak.`)
-        })
-        .catch(err => ctx.reply("Eksicode sunucularıyla bağlantı kuramıyoruz. Lütfen daha sonra deneyin."))
+bot.command('kaynak', (ctx) => {
+    fetch("http://api.eksicode.org/auth/local", {
+      'method': 'POST',
+      'body': JSON.stringify({
+        'identifier': process.env.API_USER,
+        'password': process.env.API_PASS
       })
-      .catch(err => {
-        if(err.name == "TypeError") {
-          ctx.reply("Geçersiz kullanım. Kullanım: /kaynak <link>")
-        } else if (err.name == "FetchError") {
-          ctx.reply("Link geçersiz. Lütfen tekrar deneyin.")
-        }
-      })
+    })
+    .then(res => res.json())
+    .then(info => {
+      const args = ctx.state.command.splitArgs;
+      const url = args[0].startsWith("http") ? args[0] : "http://" + args[0]
+      fetch(url)
+        .then(res => res.text())
+        .then(html => {
+          const title = parse(html).querySelector('title').rawText;
+          const requestData = {
+            "doc_name": title,
+            "doc_link": url,
+            "doc_creator_tg": ctx.from.id
+          }
+          fetch("http://api.eksicode.org/kaynaklars", {
+            "method": "POST",
+            "headers": {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + info.jwt
+            },
+            "body": JSON.stringify(requestData)
+          })
+          .then(res => res.json())
+          .then(() => {
+            ctx.reply(`Teşekkürler ${ctx.from.first_name}! Yönetici ekibimiz en kısa zamanda inceleyip onaylayacak.`)
+          })
+          .catch(err => ctx.reply("Eksicode sunucularıyla bağlantı kuramıyoruz. Lütfen daha sonra deneyin."))
+        })
+        .catch(err => {
+          if(err.name == "TypeError") {
+            ctx.reply("Geçersiz kullanım. Kullanım: /kaynak <link>")
+          } else if (err.name == "FetchError") {
+            ctx.reply("Link geçersiz. Lütfen tekrar deneyin.")
+          }
+        })
+    })
 });
 
 bot.on(["new_chat_members", "left_chat_member"], removeJoinedHandler)
