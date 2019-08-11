eb5798d352e932d1882ee6c13cb3f6e384a76713
AtakanErmis <ataerms@gmail.com>
2019-08-11 23:51:27 +0300

    kanal botu herkese açık

--- functions/commands/kanal.js
+++ functions/commands/kanal.js
@@ -5,29 +5,27 @@ const { parse } = require("node-html-parser");
 const kanalCommand = ctx => {
     const args = ctx.state.command.args;
     ctx.getChatMember(ctx.from.id).then(res => {
-        if (res.status != "member") {
-            fetch(`http://api.eksicode.org/telegrams`)
-                .then(res => res.json())
-                .then(channels => {
-                    const fuse = new Fuse(channels, {
-                        shouldSort: true,
-                        threshold: 0.3,
-                        minMatchCharLength: 1,
-                        keys: ["name"]
-                    });
-                    const searchResults = fuse.search(args);
-                    if (searchResults.length) {
-                        ctx.reply(
-                            `Sonuçlar:
+        fetch(`http://api.eksicode.org/telegrams`)
+            .then(res => res.json())
+            .then(channels => {
+                const fuse = new Fuse(channels, {
+                    shouldSort: true,
+                    threshold: 0.3,
+                    minMatchCharLength: 1,
+                    keys: ["name"]
+                });
+                const searchResults = fuse.search(args);
+                if (searchResults.length) {
+                    ctx.reply(
+                        `Sonuçlar:
                         \n${searchResults
                             .map(e => `${e.name}: ${e.link}\n`)
                             .join("")}`
-                        );
-                    } else {
-                        ctx.reply("Wow! Hiç sonuç yok.");
-                    }
-                });
-        }
+                    );
+                } else {
+                    ctx.reply("Wow! Hiç sonuç yok.");
+                }
+            });
     });
 };
 
