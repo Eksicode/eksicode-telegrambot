b8ef0180670ad7cd8ce14c745b9a86599ac5f044
AtakanErmis <ataerms@gmail.com>
2019-08-12 16:45:24 +0300

    debug amaçlı kod kaldırıldı

--- telegram.js
+++ telegram.js
@@ -19,7 +19,6 @@ setInterval(() => fn.fetchUserCount(bot), 3600000);
 bot.command("kaynak", ctx => fn.cmd.kaynakCommand(ctx));
 bot.command("kanal", ctx => fn.cmd.kanalCommand(ctx));
 bot.command("pinle", ctx => fn.cmd.pinMessage(ctx));
-bot.command("id", ctx => ctx.reply(ctx.message.chat.id));
 bot.command("ban", ctx => fn.cmd.banCommand(ctx));
 
 bot.on(["new_chat_members", "left_chat_member"], fn.joinedLeftUserHandler);
