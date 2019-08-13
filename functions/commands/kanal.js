9265e61326a78d441d1578b5686e73a729b3d529
AtakanErmis <ataerms@gmail.com>
2019-08-13 18:39:56 +0300

    /kanal tümü, markdown linkler

--- functions/commands/kanal.js
+++ functions/commands/kanal.js
@@ -6,22 +6,29 @@ function kanalCommand(ctx) {
     fetch(`http://api.eksicode.org/telegrams`)
         .then(res => res.json())
         .then(channels => {
-            const fuse = new Fuse(channels, {
-                shouldSort: true,
-                threshold: 0.3,
-                minMatchCharLength: 1,
-                keys: ["name"]
-            });
-            const searchResults = fuse.search(args);
-            if (searchResults.length) {
-                ctx.reply(
-                    `Sonuçlar:
+            if (args == "tümü") {
+                ctx.replyWithMarkdown(
+                    `Kanallar:
+                    \n${channels.map(e => `[${e.name}](${e.link})\n`).join("")}`
+                );
+            } else {
+                const fuse = new Fuse(channels, {
+                    shouldSort: true,
+                    threshold: 0.3,
+                    minMatchCharLength: 1,
+                    keys: ["name"]
+                });
+                const searchResults = fuse.search(args);
+                if (searchResults.length) {
+                    ctx.reply(
+                        `Sonuçlar:
                         \n${searchResults
                             .map(e => `${e.name}: ${e.link}\n`)
                             .join("")}`
-                );
-            } else {
-                ctx.reply("Wow! Hiç sonuç yok. Kullanım: /kanal <sorgu>");
+                    );
+                } else {
+                    ctx.reply("Wow! Hiç sonuç yok. Kullanım: /kanal <sorgu>");
+                }
             }
         });
 }
