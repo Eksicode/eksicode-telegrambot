da713f47dd14550a7952c34fd9167f9cffc06a1e
Atakan Ermiş <ataerms@gmail.com>
2019-11-02 21:51:29 +0300

    Docker çalışıyor, kaynak komutu inline/reply keyboard ile yeniden yazıldı, kaynak arama robotu, kod refactor

--- functions/commands/kaynak.js
+++ functions/commands/kaynak.js
@@ -1,74 +1,198 @@
 const fetch = require("node-fetch");
 const { parse } = require("node-html-parser");
+const apiAuth = require("../apiAuth");
+const Markup = require("telegraf/markup");
+
+const state = { user: {}, pagination: {} };
 
 function kaynakCommand(ctx) {
-    fetch("http://api.eksicode.org/auth/local", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            identifier: process.env.API_USER,
-            password: process.env.API_PASS
-        })
-    })
-        .then(res => res.json())
-        .then(info => {
-            const args = ctx.state.command.splitArgs;
-            const url = args[0].startsWith("http")
-                ? args[0]
-                : "http://" + args[0];
-            fetch(url)
-                .then(res => res.text())
-                .then(html => {
-                    let title
-                    try {
-                        title = parse(html).querySelector("title").rawText;
-                    } catch {
-                        title = url
-                    }
-                    const requestData = {
-                        doc_name: title,
-                        doc_link: url,
-                        doc_creator_tg: ctx.message.from.id,
-                        doc_tg_ch: ctx.message.chat.id.toString()
-                    };
-                    fetch("http://api.eksicode.org/kaynaklars", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: "Bearer " + info.jwt
-                        },
-                        body: JSON.stringify(requestData)
-                    })
-                        .then(res => res.json())
-                        .then(out => {
-                            if (out.error) {
-                                ctx.reply(`Hata ${out.statusCode}: Eksicode sunucularıyla bağlantı kuramıyoruz. Lütfen daha sonra deneyin.`)
-                            }
-                            ctx.reply(
-                                `Teşekkürler ${
-                                    ctx.from.first_name
-                                }! Yönetici ekibimiz en kısa zamanda inceleyip onaylayacak.`
-                            );
-                        })
-                        .catch(err =>
-                            ctx.reply(
-                                "Eksicode sunucularıyla bağlantı kuramıyoruz. Lütfen daha sonra deneyin."
-                            )
-                        );
-                })
-                .catch(err => {
-                    if (err.name == "TypeError") {
-                        ctx.reply(
-                            "Geçersiz kullanım. Kullanım: /kaynak <link>"
-                        );
-                        console.error(err)
-                    } else if (err.name == "FetchError") {
-                        ctx.reply("Link geçersiz. Lütfen tekrar deneyin.");
-                    }
-                });
-        });
+  ctx.reply(`Ekşicode kaynak arşivine hoşgeldiniz!`, {
+    reply_to_message_id: ctx.message.message_id,
+    reply_markup: Markup.keyboard([["🔎 Kaynak Ara", "ℹ️ Kaynak Ekle"]])
+      .oneTime()
+      .selective()
+      .resize()
+  });
 }
 
-module.exports = kaynakCommand;
+function kaynakEkle(ctx) {
+  if (!state.user[ctx.from.id]) {
+    state.user[ctx.from.id] = {};
+    state.user[ctx.from.id].inlineCommand = "addResource";
+  }
+  ctx.reply(
+    'Eklemek istediğiniz kaynağın bağlantısını yapıştırın. (iptal etmek için "iptal" yazın.)',
+    {
+      reply_to_message_id: ctx.message.message_id,
+      reply_markup: Markup.forceReply().selective()
+    }
+  );
+}
+
+function kaynakAra(ctx) {
+  if (!state.user[ctx.from.id]) {
+    state.user[ctx.from.id] = {};
+    state.user[ctx.from.id].inlineCommand = "searchResource";
+  }
+  ctx.reply('Sorgunuzu yazın. (iptal etmek için "iptal" yazın.)', {
+    reply_to_message_id: ctx.message.message_id,
+    reply_markup: Markup.forceReply().selective()
+  });
+}
+
+async function kaynakEkleApi(ctx) {
+  const url = ctx.message.text.startsWith("http")
+    ? ctx.message.text
+    : "http://" + ctx.message.text;
+  if (ctx.message.text == "iptal") {
+    ctx.reply("İşleminiz iptal edildi.");
+  } else {
+    const loadingMessage = await ctx.reply("Kaynak ekleniyor...", {
+      reply_to_message_id: ctx.message.message_id
+    });
+    let title = "";
+    try {
+      const titleFetch = await fetch(url);
+      const html = await titleFetch.text();
+      title = parse(html).querySelector("title").rawText;
+    } catch {
+      ctx.telegram.deleteMessage(
+        loadingMessage.chat.id,
+        loadingMessage.message_id
+      );
+      return ctx.reply(
+        "Bir hata oluştu. Lütfen geçerli bir bağlantı gönderdiğinizden emin olun.",
+        { reply_to_message_id: ctx.message.message_id }
+      );
+    }
+    const jwt = await apiAuth();
+    const requestData = {
+      doc_name: title,
+      doc_creator_tg: ctx.from.id,
+      doc_tg_ch: ctx.message.chat.id,
+      doc_link: url
+    };
+    try {
+      const req = await fetch("http://api.eksicode.org/kaynaklars", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + jwt
+        },
+        body: JSON.stringify(requestData)
+      });
+      if (req.status == 200) {
+        ctx.telegram.deleteMessage(
+          loadingMessage.chat.id,
+          loadingMessage.message_id
+        );
+        ctx.reply(
+          "Kaynak başarıyla eklendi! Yönetici ekibimiz inceleyip onaylayacak.",
+          { reply_to_message_id: ctx.message.message_id }
+        );
+      }
+    } catch {
+      ctx.telegram.deleteMessage(
+        loadingMessage.chat.id,
+        loadingMessage.message_id
+      );
+      ctx.reply("Bir hata oluştu. Lütfen daha sonra tekrar deneyin.", {
+        reply_to_message_id: ctx.message.message_id
+      });
+    }
+  }
+}
+
+async function kaynakAraApi(ctx) {
+  if (ctx.message.text == "iptal") {
+    ctx.reply("İşleminiz iptal edildi.");
+  } else {
+    const resultArray = await pagination(ctx.message.text, 0);
+    ctx.reply(`Sayfa: 1`, {
+      reply_to_message_id: ctx.message.message_id,
+      reply_markup: Markup.inlineKeyboard(resultArray)
+    });
+  }
+}
+
+async function pageSwitch(ctx) {
+  const query = ctx.callbackQuery.message.reply_to_message.text;
+  const pageNum = parseInt(ctx.callbackQuery.data);
+  if (pageNum + 1 !== parseInt(ctx.callbackQuery.message.text.split(": ")[1])) {
+    const resultArray = await pagination(query, pageNum);
+    ctx.editMessageText(`Sayfa: ${pageNum + 1}`, {
+      reply_to_message_id: query.message_id,
+      reply_markup: Markup.inlineKeyboard(resultArray)
+    });
+  } else {
+    ctx.answerCbQuery();
+  }
+}
+
+async function pagination(query, pageNum) {
+  const data = await fetchResources(query, pageNum);
+  let resultArray = [];
+  data.resources.map(e => {
+    resultArray.push([
+      {
+        text: e.doc_name,
+        url: e.doc_link
+      }
+    ]);
+  });
+  resultArray.push([{ text: `« 1`, callback_data: `${0}` }]);
+  resultArray[resultArray.length - 1].push({
+    text: `‹ ${pageNum > 0 ? pageNum : 1}`,
+    callback_data: `${pageNum > 0 ? pageNum - 1 : 0}`
+  });
+  resultArray[resultArray.length - 1].push({
+    text: `• ${pageNum + 1} •`,
+    callback_data: `${pageNum}`
+  });
+  resultArray[resultArray.length - 1].push({
+    text: `${pageNum + 2} ›`,
+    callback_data: `${pageNum + 1}`
+  });
+  resultArray[resultArray.length - 1].push({
+    text: `${data.pageCount} »`,
+    callback_data: `${data.pageCount - 1}`
+  });
+  return resultArray;
+}
+
+async function fetchResources(query, pageNum) {
+  const request = await fetch(
+    `http://api.eksicode.org/kaynaklars?doc_name_contains=${query}&_start=${pageNum *
+      5}&_limit=5`
+  );
+  const resources = await request.json();
+  const pageCountReq = await fetch(
+    `http://api.eksicode.org/kaynaklars/count?doc_name_contains=${query}`
+  );
+  const pageCount = await pageCountReq.text();
+  return { resources, pageCount: Math.ceil(parseInt(pageCount) / 5) };
+}
+
+async function kaynakListen(ctx) {
+  if (
+    state.user[ctx.from.id] &&
+    state.user[ctx.from.id].inlineCommand == "searchResource"
+  ) {
+    state.user[ctx.from.id] = null;
+    await kaynakAraApi(ctx);
+  } else if (
+    state.user[ctx.from.id] &&
+    state.user[ctx.from.id].inlineCommand == "addResource"
+  ) {
+    state.user[ctx.from.id] = null;
+    await kaynakEkleApi(ctx);
+  }
+}
+
+module.exports = {
+  kaynakAra,
+  kaynakCommand,
+  kaynakEkle,
+  kaynakListen,
+  pageSwitch
+};
