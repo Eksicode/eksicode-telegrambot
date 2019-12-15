63a5f6ffb0627378aff2422fa358024a6850c25c
Atakan Ermiş <ataerms@gmail.com>
2019-12-15 16:33:08 +0300

    debug exposed

--- functions/commands/unban.js
+++ functions/commands/unban.js
@@ -4,8 +4,6 @@ async function unbanCommand (ctx) {
   const request = await axios.get('http://api.eksicode.org/telegrams')
   const groups = request.data
 
-  console.log(groups)
-
   try {
     const member = await ctx.telegram.getChatMember(
       process.env.ADMIN_CH_ID,
@@ -13,10 +11,8 @@ async function unbanCommand (ctx) {
     )
 
     if ((member.status === 'administrator' || member.status === 'creator') && ctx.message.reply_to_message.forward_from) {
-      console.log('begin command')
       groups.map(async e => {
         await ctx.telegram.unbanChatMember(e.channelID, ctx.message.reply_to_message.forward_from.id)
-        console.log('unbanned')
       })
     } else {
       console.log('Unban Error: Yetkisiz İşlem / Hatalı Kullanım')
