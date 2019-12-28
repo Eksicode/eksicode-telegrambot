e0341b334d841f5a2e8934544d77a92e0ff2fd8f
Atakan Ermiş <ataerms@gmail.com>
2019-12-28 21:32:11 +0300

    ufak hatalar

--- functions/handlers/jsSartMiHandler.js
+++ functions/handlers/jsSartMiHandler.js
@@ -1,11 +1,15 @@
 function jsSartMiHandler (ctx) {
-  if (ctx.message.text.match(/(js [sş]art m[ıi])/gim)) {
-    const randomNum = Math.floor(Math.random() * 1000)
-    if (randomNum > 995) {
-      ctx.reply('Değil.')
-    } else {
-      ctx.reply('Şart.')
+  try {
+    if (ctx.message.text.match(/(js [sş]art m[ıi])/gim)) {
+      const randomNum = Math.floor(Math.random() * 1000)
+      if (randomNum > 995) {
+        ctx.reply('Değil.')
+      } else {
+        ctx.reply('Şart.')
+      }
     }
+  } catch (err) {
+    console.log('unexpected error at js şart mı handler.')
   }
 }
 
