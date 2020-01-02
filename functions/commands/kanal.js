4979bc5ac8755308ec496345913d422b51dff459
Atakan Ermiş <ataerms@gmail.com>
2020-01-02 22:48:19 +0300

    hotfix

--- functions/commands/kanal.js
+++ functions/commands/kanal.js
@@ -3,31 +3,36 @@ const { errorMessage } = require('../utilities')
 
 async function kanalCommand (ctx) {
   const args = ctx.state.command.args
-  if (args) {
-    const res = await axios.get(
-      encodeURI(
+  try {
+    if (args) {
+      const res = await axios.get(
+        encodeURI(
         `http://api.eksicode.org/telegrams?name_contains=${
           args === 'tümü' || args === '*' ? '' : args
         }`
+        )
       )
-    )
-    const channels = res.data
-    if (channels.length) {
-      ctx.replyWithMarkdown(
+      const channels = res.data
+      if (channels.length) {
+        ctx.replyWithMarkdown(
         `*Sonuçlar:*
                 \n${channels.map(e => `- [${e.name}](${e.link})\n`).join('')}`
-      )
-    } else {
-      ctx.reply(
+        )
+      } else {
+        ctx.reply(
         `${errorMessage()} Hiç sonuç bulamadık. Hatalı yazmadığınızdan emin olup tekrar deneyebilirsiniz.`, {
           reply_to_message_id: ctx.message.message_id
         }
-      )
+        )
+      }
+    } else {
+      ctx.reply('Kullanım: /kanal <sorgu|tümü>', {
+        reply_to_message_id: ctx.message.message_id
+      })
     }
-  } else {
-    ctx.reply('Kullanım: /kanal <sorgu|tümü>', {
-      reply_to_message_id: ctx.message.message_id
-    })
+  } catch (err) {
+    console.log('Kanal komutunda beklenmedik bir hata oluştu.')
+    console.error(err)
   }
 }
 
