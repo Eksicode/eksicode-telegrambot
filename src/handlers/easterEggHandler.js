66dbce21b23d6c90e7f8df73a315aeff7ad43b14
Mkl Tkn <mkltkn@gmail.com>
2020-11-02 12:46:48 +0300

    - Boomer babandir easter egg added

--- src/handlers/easterEggHandler.js
+++ src/handlers/easterEggHandler.js
@@ -1,6 +1,7 @@
 function jsSartMiHandler (ctx) {
   const jsSartMiMatch = ctx.message.text.match(/(js [sş]art m[ıi])/gim)
   const iHaveAQuestionMatch = ctx.message.text.match(/bi(rşey|şey| şey|r şey|şiy| şiy|şi| şi|' şey|'şey) sor(ucam|acağım|acam|cam|ucağım|abilirmiyim|abilir miyim)/gmi)
+  const boomerMatch = ctx.message.text.match(/(boomer)/gim)
   try {
     if (jsSartMiMatch) {
       const randomNum = Math.floor(Math.random() * 1000)
@@ -11,9 +12,11 @@ function jsSartMiHandler (ctx) {
       }
     } else if (iHaveAQuestionMatch) {
       ctx.reply('Haydi, sor sor!')
+    } else if (boomerMatch) {
+      ctx.reply('Boomer babandır...')
     }
   } catch (err) {
-    console.log('unexpected error at js şart mı handler.')
+    console.log('unexpected error at easter egg handler.')
   }
 }
 
