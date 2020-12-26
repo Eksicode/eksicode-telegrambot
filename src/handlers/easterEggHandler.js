87e678a606c4225f3c6b04e04de2d301ede42bed
Işık Kaplan <isik.kaplan@outlook.com>
2020-12-26 23:35:14 +0300

    even better regex

--- src/handlers/easterEggHandler.js
+++ src/handlers/easterEggHandler.js
@@ -1,8 +1,37 @@
+const clean = (piece) => (piece
+    .replace(/((^|\n)(?:[^\/\\]|\/[^*\/]|\\.)*?)\s*\/\*(?:[^*]|\*[^\/])*(\*\/|)/g, '$1')
+    .replace(/((^|\n)(?:[^\/\\]|\/[^\/]|\\.)*?)\s*\/\/[^\n]*/g, '$1')
+    .replace(/\n\s*/g, '')
+);
+
+const regex = ({raw}, ...interpolations) => (
+    new RegExp(interpolations.reduce(
+        (regex, insert, index) => (regex + insert + clean(raw[index + 1])),
+        clean(raw[0])
+    ), "gmi")
+);
+
+// Move above to a utils.js or something
+
+const boomerRegex = regex`
+(B|ß)
+(o|о|ó|ò|ô|ȯ|ȱ|ö|ȫ|ǒ|ŏ|ō|õ|ȭ|ő|ọ|ǿ|ơ|u|ú|ù|û|ü|ǔ|ŭ|ū|ũ|ű|ů|ụ)+
+(m)+
+(ğ)*
+(e|é|è|ė|ê|ë|ě|ĕ|ē|ẽ|e|ẹ|ı|i|í|ì|i|î|ï|ǐ|ĭ|ī|ĩ|ị)+
+(r)
+`
+
+const jsSartmiRegex = regex`(js [sş]art m[ıi])`
+
+const iHaveAQuestionRegex = regex`bi(rşey|şey| şey|r şey|şiy| şiy|şi| şi|' şey|'şey) sor(ucam|acağım|acam|cam|ucağım|abilirmiyim|abilir miyim|ammı|am mı|ayım mı|ayımmı)`
+
+
 function jsSartMiHandler (ctx) {
-  // maybe compile the regex before hand, not like we have a performance problem now but...
-  const jsSartMiMatch = ctx.message.text.match(/(js [sş]art m[ıi])/gim)
-  const iHaveAQuestionMatch = ctx.message.text.match(/bi(rşey|şey| şey|r şey|şiy| şiy|şi| şi|' şey|'şey) sor(ucam|acağım|acam|cam|ucağım|abilirmiyim|abilir miyim|ammı|am mı|ayım mı|ayımmı)/gmi)
-  const boomerMatch = ctx.message.text.match(/((b|ß)(o| |0|ó|@|ö|u|œ|ô|õ|ü|o)+m(e|ı|i)+r)/gim)
+  const jsSartMiMatch = ctx.message.text.match(jsSartmiRegex)
+  const iHaveAQuestionMatch = ctx.message.text.match(iHaveAQuestionRegex)
+  const boomerMatch = ctx.message.text.match(boomerRegex)
+  // switch-case?
   try {
     if (jsSartMiMatch) {
       const randomNum = Math.floor(Math.random() * 1000)
