915f447d9f540200fa910fec3f5b3512d97ec995
Işık Kaplan <isik.kaplan@outlook.com>
2020-12-26 22:46:02 +0300

    better regex

--- src/handlers/easterEggHandler.js
+++ src/handlers/easterEggHandler.js
@@ -2,7 +2,7 @@ function jsSartMiHandler (ctx) {
   // maybe compile the regex before hand, not like we have a performance problem now but...
   const jsSartMiMatch = ctx.message.text.match(/(js [sş]art m[ıi])/gim)
   const iHaveAQuestionMatch = ctx.message.text.match(/bi(rşey|şey| şey|r şey|şiy| şiy|şi| şi|' şey|'şey) sor(ucam|acağım|acam|cam|ucağım|abilirmiyim|abilir miyim|ammı|am mı|ayım mı|ayımmı)/gmi)
-  const boomerMatch = ctx.message.text.match(/(b(o+|0+|ó+|@+|u+)m(e+|ı+|i+)r)/gim)
+  const boomerMatch = ctx.message.text.match(/((b|ß)(o| |0|ó|@|ö|u|œ|ô|õ|ü|o)+m(e|ı|i)+r)/gim)
   try {
     if (jsSartMiMatch) {
       const randomNum = Math.floor(Math.random() * 1000)
