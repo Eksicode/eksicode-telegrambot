0da6ce93cca4ab64cc7b82ae8c6ade199a2d2e8d
Işık Kaplan <isik.kaplan@outlook.com>
2020-12-26 21:51:30 +0300

    update the easter egg regexes

--- src/handlers/easterEggHandler.js
+++ src/handlers/easterEggHandler.js
@@ -1,7 +1,8 @@
 function jsSartMiHandler (ctx) {
+  // maybe compile the regex before hand, not like we have a performance problem now but...
   const jsSartMiMatch = ctx.message.text.match(/(js [sş]art m[ıi])/gim)
-  const iHaveAQuestionMatch = ctx.message.text.match(/bi(rşey|şey| şey|r şey|şiy| şiy|şi| şi|' şey|'şey) sor(ucam|acağım|acam|cam|ucağım|abilirmiyim|abilir miyim)/gmi)
-  const boomerMatch = ctx.message.text.match(/(b(o|oo|00|000|0000|00000|ooo|oooo|oooo|ooooo|ó|óó|óóó|óóóó|óóóóó|óóóóóó|@@|@@@|u|uu)m(e|ee|eee|eeee|ı|ıı|i|ii)r)/gim)
+  const iHaveAQuestionMatch = ctx.message.text.match(/bi(rşey|şey| şey|r şey|şiy| şiy|şi| şi|' şey|'şey) sor(ucam|acağım|acam|cam|ucağım|abilirmiyim|abilir miyim|ammı|am mı|ayım mı|ayımmı)/gmi)
+  const boomerMatch = ctx.message.text.match(/(b(o+|0+|ó+|@+|u+)m(e+|ı+|i+)r)/gim)
   try {
     if (jsSartMiMatch) {
       const randomNum = Math.floor(Math.random() * 1000)
