6ca26f8d77e0751ab647343a737488fc5648c17d
Mkl Tkn <mkltkn@gmail.com>
2021-02-06 11:55:17 +0300

    - Error updated

--- telegram.js
+++ telegram.js
@@ -6,7 +6,7 @@ const path = require('path')
 require('dotenv').config({
   path: path.join(__dirname, '.env')
 })
-const Telegraf = require('telegraf')
+const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const axios = require('axios')
 const { apiAuth } = require('./src/utils')
@@ -18,6 +18,7 @@ const sleep = (ms) => {
 (async () => {
   const jwt = await apiAuth()
 
+  // Update chanel members 
   const updateMembers = async (id, members) => {
     try {
       const requestData = {
@@ -30,6 +31,9 @@ const sleep = (ms) => {
         }
       }
       axios.put(`${process.env.API_URL}/telegrams/` + id, requestData, config)
+        .catch(err => {
+          console.log(err);
+        });
     } catch (err) {
       console.error(err)
     }
@@ -37,6 +41,7 @@ const sleep = (ms) => {
 
   let allGroups
 
+  // Get chanel list from api
   axios.get(`${process.env.API_URL}/telegrams?_sort=ListOrder:ASC`)
     .then(async function (response) {
       allGroups = response.data
@@ -44,22 +49,28 @@ const sleep = (ms) => {
     .then(async () => {
       console.log('toplam kanal sayısı: ' + allGroups.length)
 
+      // Get chanel member count from Telegram 
       for (let i = 0; i < allGroups.length; i++) {
         await bot.telegram
           .getChatMembersCount(allGroups[i].channelID)
           .then(data => { allGroups[i].members = data })
           .then(async () => {
+            console.log(allGroups[i].name, allGroups[i].members + ' kişi')
             await sleep(2000)
             updateMembers(allGroups[i].id, allGroups[i].members)
 
             console.log(allGroups[i].name + ' Güncellendi...')
+            console.log('-------------------------')
           })
-          .catch(() => console.log(`${allGroups[i].name} için data çekilemedi.`))
+          .catch(err => {
+            console.log(`${allGroups[i].name} için data çekilemedi.`)
+            console.log(err);
+          });
 
         delete allGroups[i].channelID
       }
     })
-    .catch((error) => {
-      console.log(error)
+    .catch((err) => {
+      console.log(err)
     })
 })()
